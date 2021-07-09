21028b9026db7572b915117761e82c8ed96d5f1c
배성현 <baeseonghyun@baeseonghyeon-ui-MacBookPro.local>
2021-07-09 17:47:40 +0900

    add Sogang-Class-Notes things

--- ComputerGraphics/HW6_20161595/4.0.1.3D_Objects_Anim_20/Tiger_Texture_PS_GLSL/Shaders/simple.frag
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec4 v_color;
-
-layout (location = 0) out vec4 final_color;
-
-void main(void) {
-    final_color = v_color;
-}
--- gososil/[고소실_5주차실습]2반_20161595_배성현/main.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include "my_solver.h"
-
-int main() {
-    program2_1(); // make pdf table
-    program2_2(); // create random number
-
-    //program2_3(); // HOMEWORK
-}
